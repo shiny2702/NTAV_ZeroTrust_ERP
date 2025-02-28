3a54fd74d04d9b48887c6f7a1480cc6f80ebdeb6
notry345 <unju4898@gmail.com>
2025-02-28 21:57:59 +0900

    renovation

--- backend/routes/envRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { generateDeviceToken } = require('../controllers/envController')
-
-const router = express.Router();
-
-router.post('/verify-device', generateDeviceToken);
-
-module.exports = router;
