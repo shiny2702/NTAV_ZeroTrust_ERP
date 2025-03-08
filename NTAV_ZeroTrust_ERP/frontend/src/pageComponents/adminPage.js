b6407f9836c80722af8af8a3072a1cef6d709068
Hanwoojung <april4326@gmail.com>
2025-03-08 20:50:38 +0900

    상단바 메뉴 중복 컴포넌트화 직전

--- NTAV_ZeroTrust_ERP/frontend/src/pageComponents/adminPage.js
+++ NTAV_ZeroTrust_ERP/frontend/src/pageComponents/adminPage.js
@@ -39,30 +39,38 @@ class AdminPage extends Component {
 
     return (
       <div className="adminPage">
-        <div className="header">
+         <div className="header">
           <div className="logo" onClick={this.handleLogoClick}>NTAV</div>
           <nav className="menu">
             <ul>
-              <li className="dropdown">경영/기획 ▾
+              <li className="dropdown">경영 ▾
                 <div className="dropdown-menu">
                   <div className="dropdown-column">
-                    <h3>영업전략</h3>
+                    <h3>영업</h3>
                     <ul>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>영업전략기획</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>영업목표설정</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>영업목표</li>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>성과평가</li>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>고객(B2B)관리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>시장조사</li>
                     </ul>
                   </div>
                   <div className="dropdown-divider"></div> {/* 세로 구분선 */}
                   <div className="dropdown-column">
-                    <h3>직영운영</h3>
+                    <h3>기획</h3>
                     <ul>
-                      <li onClick={() => this.handleMenuClick("directOperation")}>직영계약관리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>직영운영</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>매출보고</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>매출성과분석</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>영업전략기획</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>신제품/서비스기획</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>시장조사</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
+                  <div className="dropdown-column">
+                    <h3>운영</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>직영점관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>메뉴관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>매출분석및보고</li>
                     </ul>
                   </div>
                 </div>
@@ -73,18 +81,17 @@ class AdminPage extends Component {
                     <h3>재무</h3>
                     <ul>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>재무계획수립</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용및데이터분석</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>운영비관리</li>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>예산관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>재무데이터분석</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>재무요청</li>
                     </ul>
                   </div>
                   <div className="dropdown-divider"></div> {/* 세로 구분선 */}
                   <div className="dropdown-column">
                     <h3>회계</h3>
                     <ul>
-                      <li onClick={() => this.handleMenuClick("directOperation")}>회계기록유지</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>매출및지출관리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>세무</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>회계기록유지</li>    {/* 모든 돈관련 ,, 월급까지 */}
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>세무관리</li>
                     </ul>
                   </div>
                 </div>
@@ -95,7 +102,9 @@ class AdminPage extends Component {
                     <h3>HR정책</h3>
                     <ul>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>인사정책수립</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>휴가정책수립</li>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>조직구성관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>직원교육</li>
                     </ul>
                   </div>
                   <div className="dropdown-divider"></div> {/* 세로 구분선 */}
@@ -103,9 +112,25 @@ class AdminPage extends Component {
                     <h3>인사관리</h3>
                     <ul>
                       <li onClick={() => this.handleMenuClick("directOperation")}>채용관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>인력지원</li>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>급여관리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>근무평가</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>근태관리</li>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>성과평가</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className="dropdown">마케팅 ▾
+                <div className="dropdown-menu">
+                  <div className="dropdown-column">
+                    <h3>마케팅</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅시장조사</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅전략개발</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅캠페인</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅성과평가</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
                     </ul>
                   </div>
                 </div>
@@ -113,59 +138,35 @@ class AdminPage extends Component {
               <li className="dropdown">고객관리 ▾
                 <div className="dropdown-menu">
                   <div className="dropdown-column">
-                    <h3>마케팅</h3>
-                    <ul>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅전략개방</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅성과분석</li>
-                    </ul>
-                  </div>
-                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
-                  <div className="dropdown-column">
                     <h3>고객서비스</h3>
                     <ul>
                       <li onClick={() => this.handleMenuClick("directOperation")}>서비스정책수립</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>고객피드백수집및분석</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>회원관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>고객요청및피드백수집</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>고객요청및피드백분석</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
                     </ul>
                   </div>
                 </div>
               </li>
-              <li className="dropdown">물류관리 ▾
+              <li className="dropdown">물류 ▾
                 <div className="dropdown-menu">
                   <div className="dropdown-column">
                     <h3>공급망관리</h3>
-                    <ul>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>공급업체선정</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>계약협상</li>
+                    <ul> 
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>공급업체관리</li>  {/* contract도 */}
+                      <li onClick={() => this.handleMenuClick("directOperation")}>배송업체관리</li>
                       <li onClick={() => this.handleMenuClick("salesStrategy")}>구매전략수립</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용분석</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
                     </ul>
                   </div>
                   <div className="dropdown-divider"></div> {/* 세로 구분선 */}
                   <div className="dropdown-column">
                     <h3>물류</h3>
                     <ul>
-                      <li onClick={() => this.handleMenuClick("directOperation")}>구매주문처리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>재고관리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>품질관리</li>
-                    </ul>
-                  </div>
-                </div>
-              </li>
-              <li className="dropdown">관리기능 ▾
-                <div className="dropdown-menu">
-                  <div className="dropdown-column">
-                    <h3>시스템운영관리</h3>
-                    <ul>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>방화벽및보안관리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>로그관리</li>
-                    </ul>
-                  </div>
-                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
-                  <div className="dropdown-column">
-                    <h3>직원관리</h3>
-                    <ul>
-                      <li onClick={() => this.handleMenuClick("accountManagement")}>직원계정관리</li>
-                      <li onClick={() => this.handleMenuClick("salesStrategy")}>프로젝트권한관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>제품관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>주문/배송관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>품질/재고관리</li>
                     </ul>
                   </div>
                 </div>
--- NTAV_ZeroTrust_ERP/frontend/src/pageComponents/mainPage.js
+++ NTAV_ZeroTrust_ERP/frontend/src/pageComponents/mainPage.js
@@ -48,22 +48,138 @@ class MainPage extends Component {
 
     return (
       <div className="mainPage">
-        <div className="header">
+                <div className="header">
           <div className="logo" onClick={this.handleLogoClick}>NTAV</div>
           <nav className="menu">
             <ul>
-              <li>경영</li>
-              <li>재무회계</li>
-              <li className="dropdown">인사 ▾
-                <ul className="dropdown-menu">
-                  <li>채용관리</li>
-                  <li>HR데이터분석및리포팅</li>
-                  <li>급여관리</li>
-                  <li>복리후생관리</li>
-                </ul>
+              <li className="dropdown">경영 ▾
+                <div className="dropdown-menu">
+                  <div className="dropdown-column">
+                    <h3>영업</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>영업목표</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>성과평가</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>고객(B2B)관리</li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
+                  <div className="dropdown-column">
+                    <h3>기획</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>영업전략기획</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>신제품/서비스기획</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>시장조사</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
+                  <div className="dropdown-column">
+                    <h3>운영</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>직영점관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>메뉴관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>매출분석및보고</li>
+                    </ul>
+                  </div>
+                </div>
               </li>
-              <li>고객관리</li>
-              <li>물류관리</li>
+              <li className="dropdown">재무회계 ▾
+                <div className="dropdown-menu">
+                  <div className="dropdown-column">
+                    <h3>재무</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>재무계획수립</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>예산관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>재무데이터분석</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>재무요청</li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
+                  <div className="dropdown-column">
+                    <h3>회계</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>회계기록유지</li>    {/* 모든 돈관련 ,, 월급까지 */}
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>세무관리</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className="dropdown">인사 ▾
+                <div className="dropdown-menu">
+                  <div className="dropdown-column">
+                    <h3>HR정책</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>인사정책수립</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>휴가정책수립</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>조직구성관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>직원교육</li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
+                  <div className="dropdown-column">
+                    <h3>인사관리</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>채용관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>인력지원</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>급여관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>근태관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>성과평가</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className="dropdown">마케팅 ▾
+                <div className="dropdown-menu">
+                  <div className="dropdown-column">
+                    <h3>마케팅</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅시장조사</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅전략개발</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅캠페인</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>마케팅성과평가</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className="dropdown">고객관리 ▾
+                <div className="dropdown-menu">
+                  <div className="dropdown-column">
+                    <h3>고객서비스</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>서비스정책수립</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>회원관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>고객요청및피드백수집</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>고객요청및피드백분석</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
+              <li className="dropdown">물류 ▾
+                <div className="dropdown-menu">
+                  <div className="dropdown-column">
+                    <h3>공급망관리</h3>
+                    <ul> 
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>공급업체관리</li>  {/* contract도 */}
+                      <li onClick={() => this.handleMenuClick("directOperation")}>배송업체관리</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>구매전략수립</li>
+                      <li onClick={() => this.handleMenuClick("salesStrategy")}>비용처리</li>
+                    </ul>
+                  </div>
+                  <div className="dropdown-divider"></div> {/* 세로 구분선 */}
+                  <div className="dropdown-column">
+                    <h3>물류</h3>
+                    <ul>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>제품관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>주문/배송관리</li>
+                      <li onClick={() => this.handleMenuClick("directOperation")}>품질/재고관리</li>
+                    </ul>
+                  </div>
+                </div>
+              </li>
             </ul>
           </nav>
           <button className="logoutButton" onClick={this.handleLogoutClick}>
@@ -147,6 +263,3 @@ class MainPage extends Component {
 }
 
 export default withRouter(MainPage);  // withRouter로 감싸기
-
-
-
